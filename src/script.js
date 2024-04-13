43dc1da70ad3eaaa0220d4e731b89a4eec6cf71d
SohaV <vankudresoha12@gmail.com>
2024-04-13 19:28:23 -0400

    Weather detail functionality
    Fetching weather details of a city from the API call and displaying it.

---
+++ src/script.js
@@ -0,0 +1,21 @@
+const apiKey = "c82ac814f482be0ee39187e8c0632fb7";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/hr";
+}
+
+searchButton.addEventListener("click", () => {
+    checkWeather(searchBox.value)
+});
+
+
